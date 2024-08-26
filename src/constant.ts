2361a067db2bacb51a03778400063a9b50cf1286
Lokesh Kumar <lk07540@gmail.com>
2024-08-26 16:21:38 +0530

    Added eslint & prettier

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-export const EXTENSION_ID = 'circlecodesolution.ccs-flutter-color';
-export const PROXY_URL = 'http://65.1.61.75:8080';
+export const EXTENSION_ID = "circlecodesolution.ccs-flutter-color";
+export const PROXY_URL = "http://65.1.61.75:8080";
