5691e68b6d164ef8b3c7ea4fd668adbd862aed80
Lokesh Kumar <lk07540@gmail.com>
2025-12-30 23:02:02 +0530

    Updated mixpanel

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
 export const EXTENSION_ID = 'circlecodesolution.ccs-flutter-color';
-export const TRACKING_ID = '76d0935f0e70d827e0004ad9c89e35e0';
+export const TRACKING_ID = 'a1af3d4704a15dee587b50e86ab5c1cb';
