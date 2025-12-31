db60f3133b70a3d493e35cfb2181eef747850f0f
Lokesh Kumar <lk07540@gmail.com>
2025-12-31 07:19:13 +0530

    Updated mixpanel

--- src/constant.ts
+++ src/constant.ts
@@ -3,4 +3,4 @@ export const FLUTTER_COLOR_REGEX = /(0x[a-f0-9A-F]{8})/;
 export const HEX_COLOR_REGEX = /(#[a-f0-9A-F]{6,8})/;
 export const RGB_COLOR_DETECTION_REGEX = /(rgba?\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}(?:\s*,\s*(?:\d+)?\.?\d+)?\s*\))/;
 export const RGB_COLOR_PARSING_REGEX = /rgba?\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})(?:\s*,\s*((?:\d+)?\.?\d+))?\s*\)/;
-export const TRACKING_ID = '76d0935f0e70d827e0004ad9c89e35e0';
+export const TRACKING_ID = 'a1af3d4704a15dee587b50e86ab5c1cb';
